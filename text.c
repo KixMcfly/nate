e3514c8a563fe8649be748492256e53209537d67
Shawn Achimasi <sachimasi@gmail.com>
2022-08-07 17:37:25 -0400

    text borders

--- text.c
+++ text.c
@@ -1,26 +1,42 @@
 #include "text.h"
 
-static FONT *font_shut = NULL;
+static FONT *font_sis = NULL;
+static BITMAP *border = NULL;
 
 void
 text_load_font_dat (char *dn, char *fn)
 {
+	DATAFILE *df = NULL;
 	char *names[] = { fn, NULL };
+	BITMAP *temp;
 	
-	if (font_shut)
-		destroy_font (font_shut);
+	if (!border){
+		df = load_datafile_object (dn, "TXTBORDER_BMP");
+		temp = (BITMAP *)df->dat;
+		border = create_bitmap (temp->w, temp->h);
+		blit (border, temp, 0, 0, 0, 0, temp->w, temp->h);
+		
+		unload_datafile_object (df);
+	}
 	
-	font_shut = load_dat_font (dn, NULL, names);
+	if (font_sis)
+		destroy_font (font_sis);
+	
+	font_sis = load_dat_font (dn, NULL, names);
 }
 
 void
 text_destroy_font (void)
 {
-	destroy_font (font_shut);
+	destroy_bitmap (border);
+	border = NULL;
+	destroy_font (font_sis);
+	font_sis = NULL;
 }
 
 void
 text_print_center (BITMAP *b, char *t)
 {
-	textout_centre_ex (b, font_shut, t, b->w >> 1, 5, -1, -1);
+	
+	textout_ex (b, font_sis, t, b->w >> 1, 5, -1, -1);
 }
