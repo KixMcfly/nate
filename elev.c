ff9f82827571984784de560389436e7a71f2c10a
shawn <sachimasi9187@gmail.com>
2022-09-07 15:04:01 +0000

    Begin Elevator Functions

--- elev.c
+++ elev.c
@@ -1,2 +1,23 @@
 #include "elev.h"
 
+struct ELEV {
+	
+	int floors;
+	int cur_floor;
+	char **fl;
+	
+} static elev = {
+	.floors = 0,
+	.cur_floor = 0
+};
+
+int
+elev_init (char *dfn)
+{
+	DATAFILE *df = load_datafile (dfn);
+	
+	
+	
+	unload_datafile (df);
+}
+
