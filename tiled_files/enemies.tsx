daab49908ed9355ee87baeb32f5def9e940f72b5
shawn <sachimasi9187@gmail.com>
2022-09-20 02:03:57 +0000

    Added Tiled enemy stat and attack Prop Types

--- tiled_files/enemies.tsx
+++ tiled_files/enemies.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="enemies" tilewidth="120" tileheight="60" tilecount="5" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="enemies" class="ENEMY" tilewidth="115" tileheight="60" tilecount="5" columns="0" objectalignment="bottomright" fillmode="preserve-aspect-fit">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0" type="ENEMY">
+ <tile id="0" class="ENEMY">
   <properties>
    <property name="BITE" type="class" propertytype="ATTACK">
     <properties>
@@ -17,7 +17,7 @@
      <property name="prob" type="int" value="45"/>
     </properties>
    </property>
-   <property name="STAT" type="class" propertytype="STAT">
+   <property name="STATS" type="class" propertytype="STAT">
     <properties>
      <property name="health" type="int" value="100"/>
      <property name="item" type="int" propertytype="INV_LIST" value="0"/>
@@ -28,9 +28,9 @@
   </properties>
   <image width="60" height="60" source="../images/enemy/cuterat.bmp"/>
  </tile>
- <tile id="1" type="ENEMY">
+ <tile id="1" class="ENEMY">
   <properties>
-   <property name="STAT" type="class" propertytype="STAT">
+   <property name="STATS" type="class" propertytype="STAT">
     <properties>
      <property name="health" type="int" value="30"/>
      <property name="money" type="int" value="1"/>
@@ -39,7 +39,7 @@
    </property>
    <property name="pop" type="class" propertytype="ATTACK">
     <properties>
-     <property name="dam" type="int" value="1"/>
+     <property name="dam" type="int" value="-1"/>
      <property name="desc" value="%s %ss!"/>
      <property name="prob" type="int" value="20"/>
     </properties>
@@ -53,14 +53,88 @@
   </properties>
   <image width="60" height="60" source="../images/enemy/beachbld.bmp"/>
  </tile>
- <tile id="2" type="ENEMY">
+ <tile id="2" class="ENEMY">
+  <properties>
+   <property name="BLOCKS YOUR DRIVEWAY" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="8"/>
+     <property name="desc" value="%s %s!"/>
+     <property name="prob" type="int" value="10"/>
+    </properties>
+   </property>
+   <property name="LOSES YOUR MAIL" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="2"/>
+     <property name="desc" value="%s %sES!"/>
+     <property name="prob" type="int" value="30"/>
+    </properties>
+   </property>
+   <property name="STATS" type="class" propertytype="STAT">
+    <properties>
+     <property name="health" type="int" value="150"/>
+     <property name="money" type="int" value="8"/>
+     <property name="name" value="Angry Mailman"/>
+    </properties>
+   </property>
+  </properties>
   <image width="40" height="40" source="../images/enemy/madmail.bmp"/>
  </tile>
- <tile id="3" type="ENEMY">
+ <tile id="3" class="ENEMY">
+  <properties>
+   <property name="LOSES PACKETS" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="-8"/>
+     <property name="desc" value="%s %s!"/>
+     <property name="prob" type="int" value="70"/>
+    </properties>
+   </property>
+   <property name="STATS" type="class" propertytype="STAT">
+    <properties>
+     <property name="health" type="int" value="90"/>
+     <property name="money" type="int" value="10"/>
+     <property name="name" value="Broken Router"/>
+    </properties>
+   </property>
+   <property name="WARRENY IS EXPIRED" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="-10"/>
+     <property name="desc" value="%ss %s!"/>
+     <property name="prob" type="int" value="20"/>
+    </properties>
+   </property>
+  </properties>
   <image width="40" height="40" source="../images/enemy/router.bmp"/>
  </tile>
- <tile id="4" type="ENEMY">
+ <tile id="4" x="0" y="0" width="115" height="60" class="ENEMY">
   <properties>
+   <property name="EAT" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="-1"/>
+     <property name="desc" value="%s %sS on your %s!"/>
+     <property name="prob" type="int" value="10"/>
+    </properties>
+   </property>
+   <property name="POOP" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="-20"/>
+     <property name="desc" value="%s %sS on your head!"/>
+     <property name="prob" type="int" value="20"/>
+    </properties>
+   </property>
+   <property name="RETARD STRENGTH WIND MILL" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="-50"/>
+     <property name="desc" value="%s performs %s!"/>
+     <property name="prob" type="int" value="10"/>
+    </properties>
+   </property>
+   <property name="SMACK" type="class" propertytype="ATTACK">
+    <properties>
+     <property name="dam" type="int" value="-15"/>
+     <property name="desc" value="%s %sS you on your head!"/>
+     <property name="prob" type="int" value="40"/>
+    </properties>
+   </property>
    <property name="STATS" type="class" propertytype="STAT">
     <properties>
      <property name="health" type="int" value="3000"/>
